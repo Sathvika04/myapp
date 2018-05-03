8a63e61a2eb581208f49743b22286065937bd5a0
sathvika Narneni <sathvikanarneni@sathvikas-mbp.home>
2018-05-02 22:40:06 -0500

    3rd commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello sprint1 Edited!'))
+app.get('/', (req, res) => res.send('Hello Edited!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
